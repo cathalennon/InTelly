f551739ed7511cd51866464a40b9d9081e5ba79b
Cathal Lennon <cathalennon@gmail.com>
2016-10-19 21:16:22 +1300

    First Commit

---
+++ InTellyTests/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Xunit;
+using WebApplication1.Controllers;
+
+namespace PortfolioTests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
